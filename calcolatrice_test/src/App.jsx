e79e35fa1af2ee7bfc4d5d35125a280972d40b85
Loll3r1no <lorenzo.morici2004@gmail.com>
2025-01-29 15:20:05 +0100

    calcolatrice completata

--- calcolatrice_test/src/App.jsx
+++ calcolatrice_test/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [risultato, setRisultato] = useState(0);
+  const [a, setA] = useState();
+  const [b, setB] = useState();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='container'>
+        <div className='first-number'><input type="number" placeholder='primo numero' value={a} onChange={(e) => setA(e.target.value)}/></div>
+        <div className='second-number'><input type="number" placeholder='secondo numero' value={b} onChange={(e) => setB(e.target.value)}/></div>
+        <div className="operations">
+          <button onClick={() => setRisultato(isNaN(a) || isNaN(b) ? 'Errore' : !!a + !!b)}>+</button>
+          <button onClick={() => setRisultato(isNaN(a) || isNaN(b) ? 'Errore' : a - b)}>-</button>
+          <button onClick={() => setRisultato(isNaN(a) || isNaN(b) ? 'Errore' : a * b)}>*</button>
+          <button onClick={() => setRisultato(isNaN(a) || isNaN(b) || b !== 0 ? 'Errore' : a / b)}>/</button>
+        </div>
+        <div className="risultato">{risultato}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
